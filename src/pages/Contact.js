0aef9a1df0c0daef41280bcad711460dd4e56b9d
daniel <danlikestech@gmail.com>
2022-02-12 20:04:44 -0600

    added contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,46 +1,24 @@
-import React, { useState } from "react";
-import validator from "validator";
-
 const Contact = () => {
-  const [emailError, setEmailError] = useState("");
-  const validateEmail = (e) => {
-    var email = e.target.value;
-
-    if (validator.isEmail(email)) {
-      setEmailError("Valid Email :)");
-    } else {
-      setEmailError("Enter valid Email!");
-    }
-  };
-
   return (
-    <div>
+    <div className="form">
       <h1>Contact Page</h1>
-      <div
-        style={{
-          margin: "auto",
-          marginLeft: "300px",
-        }}
-      >
-        <pre>
-          <h2>Contact Us</h2>
-          <span>Enter Email: </span>
-          <input
-            type="text"
-            id="userEmail"
-            onChange={(e) => validateEmail(e)}
-          ></input>{" "}
-          <br />
-          <span
-            style={{
-              fontWeight: "bold",
-              color: "red",
-            }}
-          >
-            {emailError}
-          </span>
-        </pre>
-      </div>
+      <form>
+        <label>First Name</label>
+        <input type="text" required></input>
+
+        <label>Last Name</label>
+        <input type="text" required></input>
+
+        <label>Subject</label>
+        <input type="text" required></input>
+
+        <label>Email</label>
+        <input type="email" required></input>
+
+        <label>Message</label>
+        <textarea required></textarea>
+        <button className="btn">Send Message</button>
+      </form>
     </div>
   );
 };
